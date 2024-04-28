1ba2c62d000344622cec03f58934803df1c1f621
itkrab <krabanium@gmail.com>
2024-04-28 22:44:29 +0300

    Changed data schema, changed a bit savingToDB, added new data adding,
    changed data schema from test to full data, changed saving to DB logging to not crash if error occured, created front functionality to add new data to DB, photo send from web-interface are in json base64 datatype

--- DB/workerSchema.js
+++ DB/workerSchema.js
@@ -1,11 +1,51 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema
 
+
 const workerSchema = new Schema({
     name: {
         type : String,
         required : true
-    }
+    },
+    surname: {
+        type : String,
+        required : true
+    },
+    patronymic: {
+        type : String,
+        required : true
+    },
+    jobTitle: {
+        type:[String],
+        default: "немає інформації"
+    },
+    phoneNumber: {
+        type : String,
+        required : true
+    },
+    email:{
+        type : String,
+        required : false,
+        default : "немає"
+    },
+    payment: {
+        type : Number,
+        required : true
+    },
+    dateOfJobStart: {
+        type: String,
+        required: true
+    },
+    address: {
+        type : String,
+        required : false
+    },
+    photo : {
+        type: Buffer,
+        required: false
+    },
+    additional : [Schema.Types.Mixed]
+
 })
 
 const Worker = mongoose.model("worker",workerSchema);
--- front/js/addNewData.js
+++ front/js/addNewData.js
@@ -1,20 +1,78 @@
-let inputName = document.getElementById("name");
-let inputSubmit = document.getElementById("submit");
+let inputSubmit = document.getElementById("sendFormButton");
+
+let data = {};
 
 
+// проверка на размер фото
+document.querySelector(".photo input").onchange = function() {
+    if(this.files[0].size > 16000000) {
+       alert("File is too big!");
+       this.value = "";
+    }
+};
 
+// проверка на заполнение обезательных данных
+function checkAllInputs(){
+    let dataGood = true;
+    let inputsNames = ['name','surname','patronymic','jobTitle','phoneNumber','payment','dateOfJobStart']
+    document.querySelector(".name input").value
+    inputsNames.forEach((item) => {
+        if (document.querySelector(`.${item} input`).value == "") {
+            document.querySelector(`.${item}`).style.border = "red solid 2px"
+            dataGood = false;
+        } else {
+            document.querySelector(`.${item}`).style.border = "red solid 0px"
+        }
+    })
+    return dataGood
+
+}
+// нажатие кнопки
 inputSubmit.addEventListener('click', ()=>{
-    console.log(inputName.value);
-        fetch("http://localhost:3000/addingNewData",{
+    // добавить проверку   
+    if(!checkAllInputs()){
+        return
+    }  
+    
+    convertImageToBase64(document.querySelector(".photo input").files[0],updateData,sendData);
+})
+
+//конвертирует фото в base64 и вызывает callback
+function convertImageToBase64(file,callback,callback2){ //document.querySelector(".photo input").files[0]
+    let fileReader = new FileReader();
+    fileReader.readAsDataURL(file);
+    fileReader.onload = function(){
+        callback(fileReader.result,callback2)
+    }
+}
+// собираем данные
+function updateData (photoDataBase64,callback) {
+    data = {
+        name: document.querySelector(".name input").value,
+        surname: document.querySelector(".surname input").value,
+        patronymic: document.querySelector(".patronymic input").value,
+        jobTitle: document.querySelector(".jobTitle input").value,
+        phoneNumber: document.querySelector(".phoneNumber input").value,
+        email: document.querySelector(".email input").value,
+        payment: document.querySelector(".payment input").value,
+        dateOfJobStart: document.querySelector(".dateOfJobStart input").value,
+        address: document.querySelector(".address input").value,
+        photo: photoDataBase64,
+        additional: document.querySelector(".additional input").value
+    }
+    console.log(data)
+    callback(data);
+}
+
+//отправка данных
+function sendData (data) {
+    fetch("http://localhost:3000/addingNewData",{
         method : "post",
         headers: {
             "Accept": 'application/json',
             "Content-Type" : "application/json"
         },
-        body : JSON.stringify({
-            name: inputName.value
-        })
-    }).then(response => console.log(response));
-})
-
+        body : JSON.stringify(data)
+    });
+}
 
